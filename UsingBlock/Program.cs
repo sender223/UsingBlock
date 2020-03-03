bc0e289e7ab2c35ff072f7569001afb7461ce5e7
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-03-03 09:28:28 -0300

    Usando o Bloco Using Part 1

--- UsingBlock/Program.cs
+++ UsingBlock/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace UsingBlock {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            string path = @"c:\temp\file1.txt";
+
+            try {
+                //quando usamos o bloco using, nós
+                //instanciamos os nossos recursos
+                //e tudo que estiver dentro do bloco
+                //será executado e logo após, na saída do bloco
+                //todos os recursos serão fechados!
+                using (FileStream fs = new FileStream(path, FileMode.Open)) {
+                    //podemos usar cascatas de blocos using. 
+                    using (StreamReader sr = new StreamReader(fs)) {
+                        while (!sr.EndOfStream) {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }//final do subbloco using
+                }//final do bloco using
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
